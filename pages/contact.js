419ee19857f938aa8b30525c1e912526d41d3821
shahid37 <shahidhumeed@gmail.com>
2020-12-28 14:25:05 +0500

    add footer component and clean code

--- pages/contact.js
+++ pages/contact.js
@@ -1,3 +1,4 @@
+import Footer from './components/footer';
 import NavBar from './components/navebar';
 
 var React = require('react');
@@ -426,123 +427,7 @@ export default class Contact extends React.Component {
 						</div>
 					</div>
 				</div>
-				<div className="footer">
-					<div className="footer__container">
-						<div className="footer__nav">
-							<a href="index.html" className="footer__nav__brand w-inline-block">
-								<img
-									src="https://assets.website-files.com/5e67db0c1e7a468249544a75/5e6ad55212785322a62948a6_logo-ir%402x.png"
-									width={81}
-									alt="Infinite Red log in light gray"
-								/>
-							</a>
-							<a href="work.html" className="footer__nav__link">
-								Work
-							</a>
-							<a href="capabilities.html" className="footer__nav__link">
-								Capabilities
-							</a>
-							<a href="community.html" className="footer__nav__link">
-								Community
-							</a>
-							<a href="company.html" className="footer__nav__link">
-								Company
-							</a>
-							<a href="http://shift.infinite.red/" target="_blank" className="footer__nav__link">
-								Blog
-							</a>
-							<a href="contact.html" aria-current="page" className="footer__nav__link w--current">
-								Contact
-							</a>
-						</div>
-						<div className="footer__contact">
-							<div className="footer__contact__locations">
-								<div className="footer__contact__locations__text">Fully remote throughout the U.S.</div>
-							</div>
-							<div className="footer__contact__info">
-								<div className="footer__contact__methods">
-									<a href="tel:+14152956152" className="footer__contact__method__link">
-										p:&nbsp;(415) 295-6152
-									</a>
-									<a
-										href="mailto:hello@infinite.red?subject=New%20message%20from%20infinite.red"
-										className="footer__contact__method__link"
-									>
-										e:&nbsp;hello@infinite.red
-									</a>
-								</div>
-								<div className="footer__contact__social">
-									<a
-										href="https://twitter.com/infinite_red"
-										target="_blank"
-										className="footer__contact__social__icon w-inline-block"
-									>
-										<img
-											src="https://assets.website-files.com/5e67db0c1e7a468249544a75/5e6ad5521278536f7a2948a2_icon-twitter-footer%402x.png"
-											width={20}
-											alt="Twitter icon"
-										/>
-									</a>
-									<a
-										href="https://www.instagram.com/infinitered_designers/"
-										target="_blank"
-										className="footer__contact__social__icon w-inline-block"
-									>
-										<img
-											src="https://assets.website-files.com/5e67db0c1e7a468249544a75/5eb3209221f6df4423f5786e_c-insta.svg"
-											width={20}
-											alt="Instagram icon"
-										/>
-									</a>
-									<a
-										href="https://github.com/infinitered"
-										target="_blank"
-										className="footer__contact__social__icon w-inline-block"
-									>
-										<img
-											src="https://assets.website-files.com/5e67db0c1e7a468249544a75/5eb3209204b0b68175029ead_c-github.svg"
-											width={20}
-											alt="GitHub icon"
-										/>
-									</a>
-									<a
-										href="https://www.youtube.com/channel/UCwpSzVt7QpLDbCnPXqR97-g"
-										target="_blank"
-										className="footer__contact__social__icon w-inline-block"
-									>
-										<img
-											src="https://assets.website-files.com/5e67db0c1e7a468249544a75/5eb32091cf9abc54fdfec0f5_c-youtube.svg"
-											width={20}
-											alt="YouTube icon"
-										/>
-									</a>
-									<a
-										href="https://www.facebook.com/infiniteredinc/"
-										target="_blank"
-										className="footer__contact__social__icon w-inline-block"
-									>
-										<img
-											src="https://assets.website-files.com/5e67db0c1e7a468249544a75/5eb32091fdaa86845c72da3e_c-facebook.svg"
-											width={20}
-											alt="Facebook icon"
-										/>
-									</a>
-									<a
-										href="https://www.linkedin.com/company/infinitered"
-										target="_blank"
-										className="footer__contact__social__icon w-inline-block"
-									>
-										<img
-											src="https://assets.website-files.com/5e67db0c1e7a468249544a75/5eb320922c43276f58d1a0f3_c-linkedin.svg"
-											width={20}
-											alt="LinkedIn icon"
-										/>
-									</a>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
+				<Footer />
 				{/*[if lte IE 9]><![endif]*/}
 				{/* Mirrored from infinite.red/contact by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 23 Dec 2020 11:36:54 GMT */}
 			</div>
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
-import axios from 'axios';
-import Link from 'next/link';
+import Footer from './components/footer';
 import NavBar from './components/navebar';
 var React = require('react');
 
@@ -185,8 +184,28 @@ const App = ({ data, error }) => {
 						</div>
 					</a>
 					<NavBar />
-					<div data-w-id="48941a4f-8c15-7a61-3715-67421ac8b6b6" className="menu-button w-nav-button">
+					<div
+						data-w-id="48941a4f-8c15-7a61-3715-67421ac8b6b6"
+						className="menu-button w-nav-button"
+						aria-label="menue"
+						role="button"
+					>
 						<div className="nav-icon">
+							<div
+								data-w-id="48941a4f-8c15-7a61-3715-67421ac8b6b9"
+								className="nav-icon__bar--white"
+								style={{ 'will-change': 'background', 'background-color': 'rgb(255, 255, 255)' }}
+							></div>
+							<div
+								data-w-id="48941a4f-8c15-7a61-3715-67421ac8b6b9"
+								className="nav-icon__bar--white"
+								style={{ 'will-change': 'background', 'background-color': 'rgb(255, 255, 255)' }}
+							></div>
+							<div
+								data-w-id="48941a4f-8c15-7a61-3715-67421ac8b6b9"
+								className="nav-icon__bar--white"
+								style={{ 'will-change': 'background', 'background-color': 'rgb(255, 255, 255)' }}
+							></div>
 							<div data-w-id="48941a4f-8c15-7a61-3715-67421ac8b6b8" className="nav-icon__bar--white" />
 							<div data-w-id="48941a4f-8c15-7a61-3715-67421ac8b6b9" className="nav-icon__bar--white" />
 							<div data-w-id="48941a4f-8c15-7a61-3715-67421ac8b6ba" className="nav-icon__bar--white" />
@@ -196,6 +215,7 @@ const App = ({ data, error }) => {
 					</div>
 				</div>
 			</div>
+
 			<div className="hero-home">
 				<div className="container">
 					<div className="hero-home__overview">
@@ -1221,127 +1241,7 @@ const App = ({ data, error }) => {
 					</a>
 				</div>
 			</div>
-			<div className="footer">
-				<div className="footer__container">
-					<div className="footer__nav">
-						<a
-							href="index.html"
-							aria-current="page"
-							className="footer__nav__brand w-inline-block w--current"
-						>
-							<img
-								src="https://assets.website-files.com/5e67db0c1e7a468249544a75/5e6ad55212785322a62948a6_logo-ir%402x.png"
-								width={81}
-								alt="Infinite Red log in light gray"
-							/>
-						</a>
-						<a href="work.html" className="footer__nav__link">
-							Work
-						</a>
-						<a href="capabilities.html" className="footer__nav__link">
-							Capabilities
-						</a>
-						<a href="community.html" className="footer__nav__link">
-							Community
-						</a>
-						<a href="company.html" className="footer__nav__link">
-							Company
-						</a>
-						<a href="http://shift.infinite.red/" target="_blank" className="footer__nav__link">
-							Blog
-						</a>
-						<a href="contact.html" className="footer__nav__link">
-							Contact
-						</a>
-					</div>
-					<div className="footer__contact">
-						<div className="footer__contact__locations">
-							<div className="footer__contact__locations__text">Fully remote throughout the U.S.</div>
-						</div>
-						<div className="footer__contact__info">
-							<div className="footer__contact__methods">
-								<a href="tel:+14152956152" className="footer__contact__method__link">
-									p:&nbsp;(415) 295-6152
-								</a>
-								<a
-									href="mailto:hello@infinite.red?subject=New%20message%20from%20infinite.red"
-									className="footer__contact__method__link"
-								>
-									e:&nbsp;hello@infinite.red
-								</a>
-							</div>
-							<div className="footer__contact__social">
-								<a
-									href="https://twitter.com/infinite_red"
-									target="_blank"
-									className="footer__contact__social__icon w-inline-block"
-								>
-									<img
-										src="https://assets.website-files.com/5e67db0c1e7a468249544a75/5e6ad5521278536f7a2948a2_icon-twitter-footer%402x.png"
-										width={20}
-										alt="Twitter icon"
-									/>
-								</a>
-								<a
-									href="https://www.instagram.com/infinitered_designers/"
-									target="_blank"
-									className="footer__contact__social__icon w-inline-block"
-								>
-									<img
-										src="https://assets.website-files.com/5e67db0c1e7a468249544a75/5eb3209221f6df4423f5786e_c-insta.svg"
-										width={20}
-										alt="Instagram icon"
-									/>
-								</a>
-								<a
-									href="https://github.com/infinitered"
-									target="_blank"
-									className="footer__contact__social__icon w-inline-block"
-								>
-									<img
-										src="https://assets.website-files.com/5e67db0c1e7a468249544a75/5eb3209204b0b68175029ead_c-github.svg"
-										width={20}
-										alt="GitHub icon"
-									/>
-								</a>
-								<a
-									href="https://www.youtube.com/channel/UCwpSzVt7QpLDbCnPXqR97-g"
-									target="_blank"
-									className="footer__contact__social__icon w-inline-block"
-								>
-									<img
-										src="https://assets.website-files.com/5e67db0c1e7a468249544a75/5eb32091cf9abc54fdfec0f5_c-youtube.svg"
-										width={20}
-										alt="YouTube icon"
-									/>
-								</a>
-								<a
-									href="https://www.facebook.com/infiniteredinc/"
-									target="_blank"
-									className="footer__contact__social__icon w-inline-block"
-								>
-									<img
-										src="https://assets.website-files.com/5e67db0c1e7a468249544a75/5eb32091fdaa86845c72da3e_c-facebook.svg"
-										width={20}
-										alt="Facebook icon"
-									/>
-								</a>
-								<a
-									href="https://www.linkedin.com/company/infinitered"
-									target="_blank"
-									className="footer__contact__social__icon w-inline-block"
-								>
-									<img
-										src="https://assets.website-files.com/5e67db0c1e7a468249544a75/5eb320922c43276f58d1a0f3_c-linkedin.svg"
-										width={20}
-										alt="LinkedIn icon"
-									/>
-								</a>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
+			<Footer />
 			{/*[if lte IE 9]><![endif]*/}
 			{/* Mirrored from infinite.red/ by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 23 Dec 2020 11:36:29 GMT */}
 		</div>
